704eb897ead4b5cfeb4c1b5d5b3e523d08e6a7f9
SiposZoltan63 <siposz@kkszki.hu>
2025-05-28 09:35:46 +0200

    kesz

--- feladat0528/MainWindow.xaml.cs
+++ feladat0528/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace feladat0528
 {
@@ -19,6 +23,92 @@ namespace feladat0528
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private string ConnectionString = "Server=localhost;Database=siposz;Uid=root;password=;Sslmode=None";
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var connection = new MySqlConnection(ConnectionString))
+                {
+                    string sql = "SELECT * FROM filmek";
+                    connection.Open();
+
+                    using (var cmd = new MySqlCommand(sql, connection))
+                    {
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            List<string> adatok = new List<string>();
+
+                            while (reader.Read())
+                            {
+                                adatok.Add($"{reader["filmazon"]};{reader["cim"]};{reader["ev"]};{reader["szines"]};{reader["mufaj"]};{reader["hossz"]}");
+                            }
+
+                            lbAdatok.ItemsSource = adatok;
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void lbAdatok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbAdatok.SelectedItem != null)
+            {
+                var selectedItem = lbAdatok.SelectedItem as string;
+                if (selectedItem != null)
+                {
+                    var adatok = selectedItem.Split(';');
+
+                    lbFilmAzon.Content = adatok[0];
+                    tb1.Text = adatok[1];
+                    tb2.Text = adatok[2];
+                    tb3.Text = adatok[3];
+                    tb4.Text = adatok[4];
+                    tb5.Text = adatok[5];
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                using (var connection = new MySqlConnection(ConnectionString))
+                {
+                    string sql = $"UPDATE filmek SET " +
+                                 $"cim = '{tb1.Text}', " +
+                                 $"ev = {tb2.Text}, " +
+                                 $"szines = '{tb3.Text}', " +
+                                 $"mufaj = '{tb4.Text}', " +
+                                 $"hossz = {tb5.Text} " +
+                                 $"WHERE filmazon = '{lbFilmAzon.Content}'";
+
+                    connection.Open();
+
+                    using (var cmd = new MySqlCommand(sql, connection))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    connection.Close();
+                    MessageBox.Show("A módosítás sikerült!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
